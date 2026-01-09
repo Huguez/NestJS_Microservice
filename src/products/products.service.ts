e56609a06668136ca423603d2a3106a29669e387
Carlos Huguez <carlos.huguez@sonorasoft.com>
2026-01-09 12:11:11 -0700

    delete, update and get product by Id with new migration availableColumn

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,47 +1,124 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, HttpCode, HttpStatus, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaService } from '../prisma.service';
 import { Pagination } from 'src/common/dto';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class ProductsService {
+
+   private logger = new Logger("ProductsService")
 
    constructor(
       private prisma: PrismaService
    ) { }
 
    async create(createProductDto: CreateProductDto) {
-      const product = await this.prisma.product.create({ data: createProductDto })
-      return product;
-   }
-
-   async findAll(pagination: Pagination) {
-      const { limit = 5, page = 1 } = pagination
-
-      const total = await this.prisma.product.count();
-
-      const products = await this.prisma.product.findMany({
-         skip: (page - 1) * limit,
-         take: limit
-      })
-      
-      return {
-         lastPage: Math.ceil( total/limit ),
-         totalProducts: total,
-         products,
+      try {
+         const product = await this.prisma.product.create({ data: createProductDto })
+         return product;
+      } catch (error) {
+         this.handlerExceptions(error)
       }
    }
 
-   findOne(id: number) {
-      return `This action returns a #${id} product`;
+   async findAll(pagination: Pagination) {
+      try {
+         const { limit = 5, page = 1 } = pagination
+
+         const total = await this.prisma.product.count({ where: { available: true } });
+
+         const products = await this.prisma.product.findMany({
+            where: {
+               available: true
+            },
+            skip: (page - 1) * limit,
+            take: limit
+         })
+
+         return {
+            lastPage: Math.ceil(total / limit),
+            totalProducts: total,
+            products,
+         }
+      } catch (error) {
+         this.handlerExceptions(error)
+      }
    }
 
-   update(id: number, updateProductDto: UpdateProductDto) {
-      return `This action updates a #${id} product`;
+   async findOne(id: number) {
+      try {
+         const product = await this.prisma.product.findFirst({
+            where: {
+               id,
+               available: true
+            }
+         })
+
+         if (!product) {
+            throw new NotFoundException(`Product with id: ${id}, not found`)
+         }
+
+         return {
+            product
+         }
+      } catch (error) {
+         this.handlerExceptions(error)
+      }
    }
 
-   remove(id: number) {
-      return `This action removes a #${id} product`;
+   async update(id: number, updateProductDto: UpdateProductDto) {
+      try {
+         await this.findOne(id);
+
+         return {
+            product: await this.prisma.product.update({
+               where: {
+                  id
+               },
+               data: {
+                  ...updateProductDto
+               },
+            })
+         }
+      } catch (error) {
+         this.handlerExceptions(error);
+      }
+   }
+
+   async remove(id: number) {
+      try {
+         await this.findOne(id);
+
+         return {
+            productDeleted: !!await this.prisma.product.update({
+               where: { id },
+               data: {
+                  available: false,
+               }
+            })
+         };
+      } catch (error) {
+         this.handlerExceptions(error)
+      }
+   }
+
+   private handlerExceptions(error: any) {
+
+      if (error.status === 404) {
+         throw new NotFoundException(error.message)
+      }
+
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+         if (error.code === 'P2002') {
+            throw new BadRequestException('Error: instance/attribute duplicate')
+         }
+      } else if (error instanceof Prisma.PrismaClientRustPanicError) {
+         throw new InternalServerErrorException('Error: Prisma engine fail.')
+      } else {
+         this.logger.error(error.message);
+         throw new InternalServerErrorException("Check Logs")
+      }
    }
 }
